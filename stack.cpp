471c53c196f848c40b8007aaf124fddc90638dc8
snafisahh <snafisahhh28@gmail.com>
2026-04-22 21:02:50 +0800

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,2 +1,35 @@
 #include "stack.h"
 
+void init(Stack* s) {
+    s->top = s->data - 1;
+}
+
+bool isEmpty(const Stack* s) {
+    return (s->top < s->data);
+}
+
+bool isFull(const Stack* s) {
+    return (s->top == s->data + MAX - 1);
+}
+
+void push(Stack* s, int value) {
+    if (isFull(s)) {
+        throw "stack penuh";
+    }
+    s->top++;          
+    *(s->top) = value; 
+}
+
+void pop(Stack* s) {
+    if (isEmpty(s)) {
+        throw "stack kosong";
+    }
+    s->top--;
+}
+
+int peek(const Stack* s) {
+    if (isEmpty(s)) {
+        throw "stack kosong";
+    }
+    return *(s->top);
+}
