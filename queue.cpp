c13afa5c23e1958743611f61be09f609941be47c
snafisahh <snafisahhh28@gmail.com>
2026-04-22 21:03:57 +0800

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,1 +1,43 @@
 #include "queue.h"
+
+void init(Queue* q) {
+    q->front = q->data;
+    q->rear = q->data - 1;
+}
+
+bool isEmpty(const Queue* q) {
+    return (q->rear < q->front);
+}
+
+bool isFull(const Queue* q) {
+    return (q->rear == q->data + MAX - 1);
+}
+
+void enqueue(Queue* q, int value) {
+    if (isFull(q)) {
+        throw "queue penuh";
+    }
+    q->rear++;
+    *(q->rear) = value;
+}
+
+void dequeue(Queue* q) {
+    if (isEmpty(q)) {
+        throw "queue kosong";
+    }
+    q->front++;
+}
+
+int front(const Queue* q) {
+    if (isEmpty(q)) {
+        throw "queue kosong";
+    }
+    return *(q->front);
+}
+
+int back(const Queue* q) {
+    if (isEmpty(q)) {
+        throw "queue kosong";
+    }
+    return *(q->rear);
+}
