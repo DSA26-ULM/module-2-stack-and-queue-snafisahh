2d47d24ea210b4b060ec36ea27aa6329d01e41db
snafisahh <snafisahhh28@gmail.com>
2026-04-22 21:04:51 +0800

    Update prob1.cpp

--- prob1.cpp
+++ prob1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
 #include "stack.h"
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    Stack s;
+    init(&s);
+
+    for (int i = 0; i < n; i++) {
+        string x;
+        cin >> x;
+
+        if (isdigit(x[0]) || (x[0] == '-' && x.length() > 1)) {
+            push(&s, stoi(x));
+        } 
+        else {
+            int b = peek(&s); pop(&s);
+            int a = peek(&s); pop(&s);
+
+            int hasil = 0; 
+            if (x == "+") hasil = a + b;
+            else if (x == "-") hasil = a - b;
+            else if (x == "*") hasil = a * b;
+            else if (x == "/") hasil = a / b;
+
+            push(&s, hasil);
+        }
+    }
+
+    cout << peek(&s);
+    return 0;
+}
