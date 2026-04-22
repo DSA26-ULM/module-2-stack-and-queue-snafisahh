922fd6c82ef7f8a48ca379df6ab0216307db408e
snafisahh <snafisahhh28@gmail.com>
2026-04-22 21:51:46 +0800

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -2,30 +2,32 @@
 
 void init(Queue* q) {
     q->front = q->data;
-    q->rear = q->data - 1;
+    q->rear = q->data;
 }
 
 bool isEmpty(const Queue* q) {
-    return q->rear < q->front;
+    return q->front == q->rear;
 }
 
 bool isFull(const Queue* q) {
-    return (q->rear - q->data + 1) == MAX;
+    return ((q->rear + 1 - q->data) % MAX) == (q->front - q->data);
 }
 
 void enqueue(Queue* q, int value) {
     if (isFull(q)) {
         throw "queue penuh";
     }
-    q->rear++;
+
     *(q->rear) = value;
+    q->rear = q->data + ((q->rear - q->data + 1) % MAX);
 }
 
 void dequeue(Queue* q) {
     if (isEmpty(q)) {
         throw "queue kosong";
     }
-    q->front++;
+
+    q->front = q->data + ((q->front - q->data + 1) % MAX);
 }
 
 int front(const Queue* q) {
@@ -39,5 +41,7 @@ int back(const Queue* q) {
     if (isEmpty(q)) {
         throw "queue kosong";
     }
-    return *(q->rear);
+
+    int idx = (q->rear - q->data - 1 + MAX) % MAX;
+    return q->data[idx];
 }
